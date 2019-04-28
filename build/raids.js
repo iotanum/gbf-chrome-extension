e9fe7d60569d1ed61adf88804d4e405f24a7fca7
iotanum <evalduuk@gmail.com>
2019-04-28 19:34:04 +0300

    Improve js injection and add backend support

--- build/raids.js
+++ build/raids.js
@@ -1,19 +1,64 @@
-var lstn = document.querySelector(".raids-ul");
-var node = document.createElement("LI");
-var textNode = document.createTextNode("Raid! :P");
-node.appendChild(textNode);
+let lstn = document.querySelector(".raids-ul");
 
-lstn.appendChild(node);
-// getRaids();
+let intervalID = ''
+let raidListUrl = "http://localhost:9000/getlist"
+let backend = "http://localhost:9000/raid?="
+let isFetching = false
+let bossIdList = []
+getAllBosses()
 
-// function getRaids() {
-//     var http = new XMLHttpRequest();
-//     var url = "gbf-raidfinder.aikats.us";
-//     var corsHack = "https://cors-anywhere.herokuapp.com/"
+function getBossRaids(boss) {
+    if (!isFetching) {
+        isFetching = true
+        let http = new XMLHttpRequest();
+        http.open('GET', backend + boss, true);
+        
+        http.onreadystatechange = function() {
+            if (http.readyState == XMLHttpRequest.DONE) {
 
-//     http.open("GET", corsHack + url, true);
-//     if (http.readyState == XMLHttpRequest.DONE) {
-//         console.log(http.responseText);
-//     }
-//     http.send(null);
-// }
+                let response = JSON.parse(http.responseText);
+                console.log(response)
+                for (i in response) {
+                    if (!bossIdList.includes(i))
+                    {
+                        if (bossIdList.length > 4)
+                        {
+                            bossIdList.shift()
+                            lstn.removeChild(lstn.getElementsByTagName("button")[0])
+                        }
+                        bossIdList.push(i)
+                        let button = document.createElement("button");
+                        let listItem = document.createElement("li");
+                        let spanNodeFirst = document.createElement("span");
+                        let spanNodeSecond = document.createElement("span");
+                        spanNodeFirst.innerHTML = i
+                        spanNodeSecond.innerHTML = response[i]
+                        button.appendChild(listItem);
+                        listItem.appendChild(spanNodeFirst);
+                        listItem.appendChild(spanNodeSecond);
+                        lstn.appendChild(button);
+                        button.onclick = buttonOnclick(i)
+                    }
+                }
+            }
+            isFetching = false
+        };
+
+        http.send();
+    }
+};
+function getAllBosses() {
+    let http = new XMLHttpRequest();
+    http.open('GET', raidListUrl, true);
+    
+    http.onreadystatechange = function() {
+        if (http.readyState == XMLHttpRequest.DONE) {
+
+            let response = JSON.parse(http.responseText);
+            bossList = response.list 
+            console.log(bossList)
+        }
+    };
+
+    http.send();
+};
--- popup/popup.js
+++ popup/popup.js
@@ -1,17 +1,11 @@
-let searchButton = document.querySelector('.raid-search-btn');
-
-searchButton.onclick = function() {
-    let raidId = document.querySelector('.raid-id-input').value;
-
+const buttonOnclick = (raidId) => () => {
     // chrome.tabs.executeScript({code: 'console.log("comeON")'});
     chrome.runtime.sendMessage({raid: raidId}, function(response) {
         console.log(response);
     })
-    var payload = { special_token: null, battle_key: raidId };
+    let payload = { special_token: null, battle_key: raidId };
 
-    chrome.tabs.executeScript({code: 'var payload = ' + JSON.stringify(payload) + ';'}, function() {
-        chrome.tabs.executeScript({code: 'var url = "/quest/battle_key_check";'}, function() {
-            chrome.tabs.executeScript({code: 'tryJoiningRaid(url, payload);'});
-        })
-    });
+    console.log("vykdo ir px")
+    chrome.tabs.executeScript({code: `tryJoiningRaid("/quest/battle_key_check", ${JSON.stringify(payload)});`});
+
 };
